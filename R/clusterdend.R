bda806c4f69132e1c56902697cc224b13ae24eab
Magdalena Arnal <mgdrnl@gmail.com>
2018-05-03 11:21:36 +0200

    Update clusterdend.R

--- R/clusterdend.R
+++ R/clusterdend.R
@@ -13,12 +13,12 @@ function(estimates_m=NULL, est_noctrls=NULL, picname, conditions=NULL, colors=NU
     if(estimates) {
         labels <- colnames(estimates_m)
         parameters <- setparam(labels)
-        many.clusters_V(estimates_m,resultsDir,paste(picname, "ClustersWithCtrls", sep="_"),"Clusters with ctrls", parameters=parameters, conditions=conditions, colors=colors)
+        many.clusters_V(estimates_m,resDir,paste(picname, "ClustersWithCtrls", sep="_"),"Clusters with ctrls", parameters=parameters, conditions=conditions, colors=colors)
     } 
     
     if(noctrls) {
         labels <- colnames(est_noctrls)
         parameters <- setparam(labels)
-        many.clusters_V(est_noctrls,resultsDir,paste(picname, "ClustersNoCtrls", sep="_"),"Clusters no ctrls", parameters=parameters, conditions=conditions, colors=colors)
+        many.clusters_V(est_noctrls,resDir,paste(picname, "ClustersNoCtrls", sep="_"),"Clusters no ctrls", parameters=parameters, conditions=conditions, colors=colors)
     }
 }
